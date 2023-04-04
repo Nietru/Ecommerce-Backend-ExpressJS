db1b7c71f1a4d9b91497bee1a3226c6ef0fe7bdd
Tifannie Truman <nietru143@gmail.com>
2023-04-03 21:55:39 -0400

    filled in the column info for my models

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,23 @@
-require('dotenv').config();
+// enable access to .env variables.
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  : // gets the data from our .env file:
+    new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        //sets up our connection:
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
 module.exports = sequelize;
